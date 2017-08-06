684486c637e9f59b5dd1c2b4823c84b1e5c223df
CatOfLuck <neket98@ukr.net>
2017-08-06 16:32:29 +0300

    final

--- calculate.java
+++ calculate.java
@@ -1,5 +1,9 @@
 public class calculate{
 	public static void main(String[] arg){
-		System.out.println("Calculate...");		
+		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double sum = first + second;
+		System.out.println("Summ " + sum);		
 	}
 }
